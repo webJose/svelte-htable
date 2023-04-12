27cc04456714659719d5a14324081625beb3263a
Ramirez Vargas, José Pablo <webJose@gmail.com>
2023-04-11 21:06:33 -0600

    Hoping for final fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 // Reexport your entry components here
 export { default as default } from "$lib/Htable.svelte";
 export { CaptionOrder, ItemGrouping } from "$lib/InnerHtable.svelte";
+export type { Column, Item } from "$lib/InnerHtable.svelte";
